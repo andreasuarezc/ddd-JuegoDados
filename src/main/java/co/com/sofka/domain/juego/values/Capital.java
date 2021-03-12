6093de28a70c429630e719534f286e189a74568f
Andrea <andrea0795@gmail.com>
2021-03-12 18:24:31 -0500

    Se crean reglas de negocio en el objeto de valor Capital

--- src/main/java/co/com/sofka/domain/juego/values/Capital.java
+++ src/main/java/co/com/sofka/domain/juego/values/Capital.java
@@ -1,4 +1,29 @@
 package co.com.sofka.domain.juego.values;
 
-public class Capital {
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Capital implements ValueObject<Integer> {
+    private final Integer value;
+
+    public Capital(Integer value) {
+        this.value = Objects.requireNonNull(value);
+        if(value<=0){
+            throw new IllegalArgumentException("El capital no puede ser cero");
+        }
+    }
+    //Estas son reglas de negocio
+    public Capital incrementarCapital(Integer value){
+        return new Capital(this.value + value);
+    }
+
+    public Capital disminuirCapital(Integer value){
+        return new Capital(this.value - value);
+    }
+
+    @Override
+    public Integer value() {
+        return value;
+    }
 }
